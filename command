23477ae0a40c2a8ddb1c74a00a52c90f863a6033
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2023-06-01 11:00:55 -0300

    refactor: moves command executions to app class instance

--- command
+++ command
@@ -1,18 +1,10 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Database\MigrationRunner;
 use Core\App;
 
-new App();
+$app = new App();
 
 [, $command] = $argv;
 
-switch ($command) {
-  case 'serve':
-    shell_exec('php -S localhost:8000 ./index.php');
-    break;
-  case 'migrate':
-    MigrationRunner::execute();
-    break;
-}
+$app->exec($command);
--- core/App.php
+++ core/App.php
@@ -4,6 +4,7 @@ namespace Core;
 
 use Core\Routing\Route;
 use Core\Scripts\Loader;
+use Database\MigrationRunner;
 
 class App
 {
@@ -20,5 +21,26 @@ class App
   public function load()
   {
     Loader::load(['routes', 'helpers']);
+  }
+
+  /**
+   * Executes a given command provided by the shell
+   * 
+   * @param $command The command that will be executed
+   */
+  public function exec(string $command)
+  {
+    switch ($command) {
+      case 'serve':
+        $port = env('APP_PORT');
+        $host = env('APP_HOST');
+        shell_exec("php -S $host:$port ./index.php");
+        break;
+      case 'migrate':
+        MigrationRunner::execute();
+        break;
+      default:
+        throw new \Exception('Invalid command.');
+    }
   }
 }
