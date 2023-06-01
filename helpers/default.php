990ad23d7f0a99100872a80665717e1fcc6d8db9
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2023-06-01 13:04:00 -0300

    docs: improves code comments in helper functions

--- helpers/default.php
+++ helpers/default.php
@@ -3,16 +3,35 @@
 use Core\Env\Parser;
 use Core\View\View;
 
+/**
+ * Redirects the page to a given location
+ * 
+ * @param $path The path that will be redirected
+ */
 function redirect(string $path)
 {
   header("Location: $path");
 }
 
+/**
+ * Shorthand function that helps load views
+ * 
+ * @param $path The path of the view
+ * @param $params The params passed to the view
+ * 
+ */
 function view(string $path, array $params = [])
 {
   return View::load($path, $params);
 }
 
+/**
+ * Gets a key from env file
+ * 
+ * @param $key The env file param
+ * 
+ * @return string The value of the key
+ */
 function env(string $key)
 {
   return Parser::get($key);
