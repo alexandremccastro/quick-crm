2dfd06be9522d26178d13bd532ea8c764de1dda7
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2023-06-03 15:58:21 -0300

    feat: added components for navbar and sidebar

---
+++ resources/js/components/Layout/Sidebar.vue
@@ -0,0 +1,55 @@
+<template>
+  <aside class="h-full sm:hidden lg:block text-gray-100 bg-neutral  min-w-[280px] shadow-2xl">
+    <section
+      class="flex shadow-sm h-14 justify-center border-b border-gray-100 border-opacity-20 bg-opacity-20 items-center">
+      <h1 class="text-2xl font-bold"><span class="text-gray-400">Quick</span>CRM</h1>
+    </section>
+
+
+    <nav class=" p-3">
+      <ul>
+        <li class="px-3 flex py-2 gap-5 hover:bg-black hover:bg-opacity-20 cursor-pointer rounded">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          </svg>
+
+
+          <span>Dashboard</span>
+        </li>
+
+        <li class="px-3 flex py-2 gap-5 hover:bg-black hover:bg-opacity-20 cursor-pointer rounded">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+          </svg>
+
+
+
+          <span>Customers</span>
+        </li>
+
+        <li class="px-3 flex py-2 gap-5 hover:bg-black hover:bg-opacity-20 cursor-pointer rounded">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+          </svg>
+
+
+
+          <span>Favorites</span>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Sidebar'
+})
+</script>
--- resources/js/index.js
+++ resources/js/index.js
@@ -4,10 +4,14 @@ import { createApp } from 'vue'
 
 import Login from './pages/auth/Login.vue'
 import Register from './pages/auth/Register.vue'
+import Sidebar from './components/Layout/Sidebar.vue'
+import Navbar from './components/Layout/Navbar.vue'
 import Home from './pages/Home.vue'
 
 const app = createApp()
 app.component('Login', Login)
 app.component('Register', Register)
+app.component('Sidebar', Sidebar)
+app.component('Navbar', Navbar)
 app.component('Home', Home)
 app.mount('#app')
