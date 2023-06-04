ffd363d39931e8a6a51d0763c7ba415d3a798e4c
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2023-06-03 22:18:58 -0300

    feat: added error page for 404 (not found)

---
+++ resources/views/errors/404.php
@@ -0,0 +1,6 @@
+@parent('errors.error')
+
+<div class="text-center">
+  <h1 class="text-7xl text-gray-900 uppercase">404</h1>
+  <h1 class="text-2xl text-gray-500 uppercase font-bold">Not Found</h1>
+</div>
---
+++ resources/views/errors/error.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>QuickCRM</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="/public/js/style.css" />
+</head>
+
+<body>
+  <main class="flex flex-col gap-4 justify-center items-center" id="app">
+    @child
+  </main>
+  <script type="module" src="/public/js/index.js"></script>
+</body>
+
+</html>
